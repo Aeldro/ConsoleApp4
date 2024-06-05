fb64a31275f246da71e8fc9d03043d29691b3ea8
Nolan DE PUYDT <nodepuydt@gmail.com>
2024-06-05 14:55:10 +0200

    Ajoutez des fichiers projet.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] firstArray = new int[3,3];
+            int[][] secondArray = new int[3][];
+
+            firstArray[0, 0] = 1;
+            firstArray[1, 0] = 2;
+            firstArray[2, 0] = 3;
+            firstArray[0, 1] = 2;
+            firstArray[1, 1] =
+
+        }
+    }
+}
